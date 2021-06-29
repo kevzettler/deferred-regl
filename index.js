a6dbc9dd188f433a026e70223f8d2fe945fcb377
Kev Zettler <kevzettler@gmail.com>
2021-06-29 08:50:48 -0700

    Add replication methods
    add meta data for replication

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
+function reduceKeyPaths(object){
+  if(!object) return "";
+  return Object.entries(object).reduce((accum, [key, val]) => {
+    accum += key;
+    if(Object.prototype.toString.call(val) === '[object Object]'){
+      accum += (':' + reduceKeyPaths(val));
+    }
+    return accum;
+  }, "")
+}
+
+
 module.exports = function () {
   var regl = null
   var queue = []
+  var map = {};
   var def = dfn('()')
   unset()
   def.queue = queue
+  def.map = map;
   def.setQueue = function(queueInput){
     queue = queueInput
   }
+
+  // This replicate method is used so that
+  // one deferred regl can point all its methods
+  // to another deferred instance
+  // This is used in react-regl so that the global instance
+  // can be pointed to componets as they are rendering
+  // this is super brittle and need a
+  def.replicateTo = function(target){
+    target.replicant = this;
+    // copy the queue to the target
+    this.queue.forEach((qi) => {
+      if(!qi.is_draw_command){
+        target.queue.push(qi);
+      }
+    })
+
+    // change all the references to the target
+    target.map = {...this.map};
+    Object.entries(this.map).forEach(([mk,mv]) => {
+      this[mk] = target[mk];
+    });
+
+    this.draw = target.draw
+    this.poll = target.poll
+    this.clear = target.clear;
+  }
+
+
   def.setRegl = function (r) {
     regl = r
     if (!r) return unset()
@@ -18,6 +60,29 @@ module.exports = function () {
       }
     }
     queue = []
+
+    if(this.replicant){
+      this.replicant.frame = r.frame
+      this.replicant.draw = r.draw
+      this.replicant.poll = r.poll
+      this.replicant.clear = r.clear
+      this.replicant.buffer = r.buffer
+      // overriding the these causes some failures
+      /* this.replicant.texture = r.texture
+       * this.replicant.elements = r.elements
+       * this.replicant.framebuffer = r.framebuffer
+       * this.replicant.framebufferCube = r.framebufferCube
+       * this.replicant.renderbuffer = r.renderbuffer
+       * this.replicant.cube = r.cube
+       * this.replicant.read = r.read */
+      this.replicant.hasExtension = r.hasExtension
+      this.replicant.limits = r.limits
+      this.replicant.stats = r.limits
+      this.replicant.now = r.now
+      this.replicant.destroy = r.destroy
+      this.replicant.on = r.on
+    }
+
     def.frame = r.frame
     def.draw = r.draw
     def.poll = r.poll
@@ -76,7 +141,7 @@ module.exports = function () {
     def.cube = dfn('cube')
     def.read = function () {}
     def.hasExtension = function () {}
-    def.limits = function () {}
+    def.limits = {lineWidthDims: [1,1]}
     def.stats = function () {}
     def.now = function () { return 0 }
     def.destroy = function () { queue.push(function (r) { r.destroy() }) }
@@ -87,27 +152,41 @@ module.exports = function () {
       if (key === '()' && regl) return regl(opts)
       else if (regl) return regl[key](opts)
 
-      var f = null
-      if (key === '()') {
-        queue.push(function (r) { f = r(opts) })
-      } else {
-        queue.push(function (r) { f = r[key](opts) })
-      }
+      var f = null;
+      var wrap = function(r){
+        if(key === '()') {
+          f = r(opts)
+        } else {
+          f = r[key](opts)
+        }
+      };
+      if(key === '()') wrap.is_draw_command = true;
+      wrap.key = key;
+      wrap.opts = opts;
+      wrap.queueIndex = queue.length - 1;
+      queue.push(wrap);
+
       var r = function () {
         var args = arguments
         if (!falsy(f)) {
           if (key === '()') f.apply(null,args)
           else return f
         } else {
-          queue.push(function (r) { f.apply(null,args) })
+          var applyer = function (r) { f.apply(null,args) };
+          applyer.key = key;
+          applyer.opts = opts;
+          applyer.queueIndex = queue.length - 1;
+          queue.push(applyer)
         }
       }
+      var mapKey = key+"-"+(queue.length-1)+"-"+reduceKeyPaths(opts)
+      map[mapKey] = r;
       r.key = key
       r.opts = opts
       r.queueIndex = queue.length - 1;
       r.deferred_regl_resource = true;
 
-      return r;
+      return map[mapKey];
     }
   }
   function dfnx (key, methods) {
@@ -115,21 +194,35 @@ module.exports = function () {
       if (key === '()' && regl) return regl(opts)
       else if (regl) return regl[key](opts)
 
-      var f = null
-      if (key === '()') {
-        queue.push(function (r) { f = r(opts) })
-      } else {
-        queue.push(function (r) { f = r[key](opts) })
-      }
+      var f = null;
+      var wrap = function(r){
+        if(key === '()') {
+          f = r(opts)
+        } else {
+          f = r[key](opts)
+        }
+      };
+      if(key === '()') wrap.is_draw_command = true;
+      wrap.key = key;
+      wrap.opts = opts;
+      wrap.queueIndex = queue.length - 1;
+      queue.push(wrap);
+
       var r = function () {
         var args = arguments
         if (!falsy(f)) {
           if (key === '()') f.apply(null,args)
           else return f
         } else {
-          queue.push(function (r) { f.apply(null,args) })
+          var applyer = function (r) { f.apply(null,args) };
+          applyer.key = key;
+          applyer.opts = opts;
+          applyer.queueIndex = queue.length - 1;
+          queue.push(applyer)
         }
       }
+      var mapKey = key+"-"+(queue.length-1)+"-"+reduceKeyPaths(opts);
+      map[mapKey] = r;
       r.key = key
       r.opts = opts
       r.queueIndex = queue.length - 1;
@@ -140,11 +233,18 @@ module.exports = function () {
         var m = methods[i]
         r[m] = function () {
           var args = arguments
-          if (!falsy(f)) return f[m].apply(f,args)
-          else queue.push(function () { f[m].apply(f,args) })
+          if (!falsy(f)) {
+            return f[m].apply(f,args)
+          } else {
+            var applyer = function (r) { f.apply(null,args) };
+            applyer.key = key;
+            applyer.opts = opts;
+            applyer.queueIndex = queue.length - 1;
+            queue.push(applyer)
+          }
         }
       }
-      return r
+      return map[mapKey];
     }
   }
 }
